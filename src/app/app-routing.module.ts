33ae76e480184f06fb5079b2e050d1d09b1a4c84
Miles <milesstanfield@gmail.com>
2017-04-06 17:45:17 -0400

    added starwars routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { StarwarsComponent } from './starwars/starwars.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', pathMatch: 'full', component: HomeComponent },
+  { path: 'starwars', pathMatch: 'full', component: StarwarsComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ];
 
@@ -13,7 +16,8 @@ const routes: Routes = [
   exports: [RouterModule],
   declarations: [
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    StarwarsComponent
   ]
 })
 export class AppRoutingModule { }
